1949daa1796c027d5d7e58942057bc877968b01a
Jacob I Walker <jacobisraelw3@gmail.com>
2020-04-11 16:48:21 -0600

    removed serviceworker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Map from './Map';
 //import {RadioColorGradient} from './RadioButtons'
-import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter as Router} from 'react-router-dom';
 import Route from 'react-router-dom/Route';
@@ -48,4 +47,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
